3c1fa5379d954ab1433e44071111aedbe9415bc9
shenshuu <michaelshen85@gmail.com>
2024-03-25 12:31:28 -0400

    Add get endpoint for news

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 require('dotenv').config()
+const { codes } = require('./countryCodes.js')
 const express = require('express')
+const axios = require('axios')
 const app = express()
 
-app.get('/news', (req, res) => {
-    const url = 'https://newsapi.org/v2/everything?' +
-          'q=Apple&' +
-          'from=2024-03-25&' +
-          'sortBy=popularity&' +
-          `apiKey=${process.env.NEWS_API_KEY}`;
-    const newsReq = new Request(url)
-    fetch(newsReq)
-        .then(response => console.log(response))
-        .catch(error => res.status(500).json({foo: 'bar'}))
+app.get('/news', async (req, res) => {
+    console.log(req.body)
+    try {
+        const url = `https://newsdata.io/api/1/news?apikey=
+                        ${process.env.NEWS_DATA_API_KEY}
+                        &country=${'us'}
+                        &language=en
+                        &full_content=1
+                        &image=1
+                        &prioritydomain=top
+                        `
+        const response = await axios.get(url);
+        res.json(response.data)
+    } catch {
+        res.status(500).json({error: 'unable to fetch results'})
+    }
 })
 
 
